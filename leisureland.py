4a4a2dadd55084f285102fae6ab07a53e073a285
Anthony Armfield <anthony.armfield@gmail.com>
2017-07-14 00:05:54 -0600

    Add files via upload
    Original script written to accomplish the adjustment of javascript code to implement into an image gallery.

---
+++ leisureland.py
@@ -0,0 +1,12 @@
+count = 1
+while count < 69:
+    imageString1 = '{"id":"'
+    imageString10 = imageString1 + str(count)
+    imageString2 = '","title":"","description":"","url":"..\/..\/data\/imagegallery\/2004-nuwa-hitchhiker-discover-america-33.5rltg\/'
+    imageString20 = imageString2 + str(count)
+    imageString3 = '.jpeg","thumbUrl":"..\/..\/data\/imagegallery\/2004-nuwa-hitchhiker-discover-america-33.5rltg\/'
+    imageString30 = imageString3 + str(count)
+    imageString4 = '.thumb.jpeg"},'
+    imageString40 = imageString4
+    print (imageString10,imageString20,imageString30,imageString40)
+    count = count + 1
